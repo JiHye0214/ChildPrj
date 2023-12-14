5cc5a3d558fbfc1610f2539fc265f3d677edd3ea
JiHye0214 <qkrwlgp1526@gmail.com>
2023-12-14 19:24:40 +0900

    modify href from jh

--- src/main/java/com/project/childprj/controller/UserController.java
+++ src/main/java/com/project/childprj/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.project.childprj.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class UserController {
+    @RequestMapping("/user/signUp")
+    public void test(){
+    }
 }
