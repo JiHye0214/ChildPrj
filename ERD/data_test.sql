a9f13c6e25a195e16d923c93e15a6dbe3de0ca40
JIWON CHOI <cjkywe07@hanyang.ac.kr>
2023-12-22 14:52:28 +0900

    kindergarden

--- ERD/data_test.sql
+++ ERD/data_test.sql
@@ -220,5 +220,18 @@ SELECT p.id 		  "p_id"
  LIMIT 0, 5;
 
 
+SELECT *
+  FROM kindergarden;
+
+SELECT COUNT(*)
+  FROM kindergarden;
+
+SELECT id, KINDERNAME
+  FROM kindergarden
+ WHERE KINDERNAME LIKE "%영은%";
+
+
+
+
 
 
