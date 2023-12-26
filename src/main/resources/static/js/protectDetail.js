3bbb5d2d22a1215eca9cd468fcce17e98dda5909
jang_9120 <ygjang9120@gmail.com>
2023-12-27 00:36:44 +0900

    지도

--- src/main/resources/static/js/protectDetail.js
+++ src/main/resources/static/js/protectDetail.js
@@ -45,11 +45,13 @@ geocoder.addressSearch(addr.value, function(result, status) {
 
         var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
+
         // 결과값으로 받은 위치를 마커로 표시합니다
         var marker = new kakao.maps.Marker({
             map: map,
             position: coords
         });
+
 
         // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
         map.setCenter(coords);
