6009c345c8dad7a3bae9cc3b436540d587f6a344
jang_9120 <ygjang9120@gmail.com>
2023-12-27 01:28:33 +0900

    마커

--- src/main/resources/static/js/protectDetail.js
+++ src/main/resources/static/js/protectDetail.js
@@ -34,6 +34,10 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+var imageSrc = 'https://ifh.cc/g/qQZkwx.png', // 마커이미지의 주소입니다
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다
+
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder(addr);
 
@@ -43,13 +47,16 @@ geocoder.addressSearch(addr.value, function(result, status) {
     // 정상적으로 검색이 완료됐으면
      if (status === kakao.maps.services.Status.OK) {
 
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+             coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
 
 
         // 결과값으로 받은 위치를 마커로 표시합니다
         var marker = new kakao.maps.Marker({
             map: map,
-            position: coords
+            position: coords,
+            image: markerImage
         });
 
 
--- src/main/resources/static/js/togetherDetail.js
+++ src/main/resources/static/js/togetherDetail.js
@@ -26,9 +26,9 @@ var container = document.getElementById('map-wrap'),
 		};
 
 var map = new kakao.maps.Map(container, options);
-var imageSrc = 'https://ifh.cc/g/16Hcob.png', // 이미지 파일의 상대 경로 // 이미지 파일의 상대 경로
+var imageSrc = 'https://ifh.cc/g/oxMhdm.png', // 이미지 파일의 상대 경로 // 이미지 파일의 상대 경로
  // 마커이미지의 주소입니다
-    imageSize = new kakao.maps.Size(40, 55), // 마커이미지의 크기입니다
+    imageSize = new kakao.maps.Size(50, 55), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
